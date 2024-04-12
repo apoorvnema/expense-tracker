49dba86a7599ab8e9532080ebb828ca7ce9114b4
apoorvnema <apoorvnema38@gmail.com>
2024-04-12 17:18:18 +0530

    made changes in login successful and error msgs

--- client/public/js/login.js
+++ client/public/js/login.js
@@ -8,7 +8,7 @@ form.addEventListener("submit", (e) => {
     }
     axios.post("http://127.0.0.1:3000/user/login", login)
         .then(result => {
-            alert("Successfully Logged In");
+            alert(result.data.message);
             window.location.href = "index.html"
         })
         .catch(err => alert(err.response.data.message))
--- controllers/user.js
+++ controllers/user.js
@@ -23,12 +23,12 @@ exports.logIn = async (req, res) => {
         const password = req.body.password;
         const findEmail = await User.findOne({ where: { email: email } });
         if (!findEmail) {
-            return res.status(404).json({ message: 'User does not exist' });
+            return res.status(404).json({ message: 'User not found' });
         }
         if (findEmail.password != password) {
-            return res.status(401).json({ message: 'Password is incorrect' });
+            return res.status(401).json({ message: 'User not authorized' });
         }
-        res.status(200).json(findEmail);
+        res.status(200).json({ message: 'User login successful' });
     }
     catch (error) {
         res.status(500).json({ message: 'Internal Server Error' });
